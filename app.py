ea7da45fa191b0f479d63e47a4a3fb5384322a96
Rayanne Ribeiro <mouraribeiro11@gmail.com>
2024-04-29 09:58:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -79,6 +79,6 @@ def atendimentos2():
     return make_response(jsonify(mensagem="Lista de atendimentos",dados=atendimentos))
 
 
-with app.app_context():    
-#db.create_all()
-    app.run(debug=True)
+   
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',debug=True, port=8001)
